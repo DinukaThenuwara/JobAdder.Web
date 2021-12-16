be6d817fc7cdb384a060c60d0034a7e5fc2dbb70
Dinuka Thenuwara <dthenuw@apac.corpdir.net>
2021-12-16 17:12:45 +0800

    Add Components and Services - no implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CandidateComponent } from './Components/candidate/candidate.component';
+import { JobComponent } from './Components/job/job.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CandidateComponent,
+    JobComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
